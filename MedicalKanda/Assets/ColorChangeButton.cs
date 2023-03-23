c713c161cb17d0435188143f45503653ae2d8af2
benj4814 <82573376+benj4814@users.noreply.github.com>
2023-03-23 12:21:27 +0100

    Open door when 3 buttons pressed

--- MedicalKanda/Assets/ColorChangeButton.cs
+++ MedicalKanda/Assets/ColorChangeButton.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class ColorChangeButton : MonoBehaviour
@@ -9,37 +8,34 @@ public class ColorChangeButton : MonoBehaviour
     public Material greenMaterial; // Assign your green material to this variable in the Inspector
 
     private Renderer buttonRenderer;
+    private ButtonManager buttonManager;
 
     void Start()
-{
-    buttonRenderer = GetComponent<Renderer>();
-    buttonRenderer.material = redMaterial;
-    Debug.Log("Button material set to red");
-}
-
-   void Update()
-{
-    if (Input.GetKeyDown(KeyCode.E))
     {
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        buttonRenderer = GetComponent<Renderer>();
+        buttonRenderer.material = redMaterial;
+        
+
+        buttonManager = GameObject.Find("GameManager").GetComponent<ButtonManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (hit.collider.gameObject == gameObject)
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
             {
-                buttonRenderer.material = greenMaterial;
-                Debug.Log("Button material set to green");
+                if (hit.collider.gameObject == gameObject)
+                {
+                    buttonRenderer.material = greenMaterial;
+
+                    buttonManager.ButtonPressed();
+                }
+                
             }
-            else
-            {
-                Debug.Log("Raycast hit something else");
-            }
-        }
-        else
-        {
-            Debug.Log("Raycast did not hit anything");
         }
     }
-}
 }
 
