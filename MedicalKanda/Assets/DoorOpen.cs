df1a5cc66660df8fcaea5831f9b3a51d5de0b8c6
benj4814 <82573376+benj4814@users.noreply.github.com>
2023-04-03 20:16:12 +0200

    SceneChange

--- MedicalKanda/Assets/DoorOpen.cs
+++ MedicalKanda/Assets/DoorOpen.cs
@@ -1,33 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+// Attach this script to the door gameObject
 using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
-    public GameObject door; // assign the door object to this in the inspector
-    private bool isOpen = false;
+    public GameObject objectToLookAt; // drag the object that the player needs to look at here in the inspector
+    private bool inTriggerZone = false;
+    private bool doorOpened = false;
+    private Collider doorCollider;
 
-    private void OnTriggerEnter(Collider other)
+    void Start()
+    {
+        doorCollider = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // the player has entered the trigger zone
-            if (Input.GetMouseButtonDown(0)) // check if the player has clicked the left mouse button
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == door)
-                {
-                    // the player is pointing at the door and clicked the left mouse button
-                    if (!isOpen)
-                    {
-                        // open the door by rotating it 90 degrees around its y-axis
-                        door.transform.Rotate(0f, 90f, 0f);
-                        isOpen = true;
-                    }
-                }
-            }
+            inTriggerZone = true;
+            Debug.Log("Player entered trigger zone.");
         }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inTriggerZone = false;
+            Debug.Log("Player exited trigger zone.");
+        }
+    }
+
+    void Update()
+    {
+        // Check for left mouse button click and player in trigger zone and looking at the objectToLookAt and door not already opened
+        if (inTriggerZone && Input.GetMouseButtonDown(0) && IsLookingAtObject() && !doorOpened)
+        {
+            // Rotate the door 90 degrees on the Y axis
+            transform.Rotate(new Vector3(0f, -90f, 0f));
+            Debug.Log("Door opened.");
+            
+            // Set the door as opened and disable its collider
+            doorOpened = true;
+            doorCollider.enabled = false;
+        }
+    }
+
+    private bool IsLookingAtObject()
+    {
+        // Get the direction from the camera to the objectToLookAt
+        Vector3 direction = objectToLookAt.transform.position - Camera.main.transform.position;
+
+        // Check if the direction vector is roughly equal to the camera's forward vector
+        return Vector3.Dot(direction.normalized, Camera.main.transform.forward) > 0.85f;
     }
 }
