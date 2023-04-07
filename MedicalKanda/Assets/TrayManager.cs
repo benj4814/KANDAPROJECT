9f5a73654a8604d902f175f1f1840058fa9ea7ad
benj4814 <82573376+benj4814@users.noreply.github.com>
2023-04-07 11:45:57 +0200

    Level 1 finished

--- MedicalKanda/Assets/TrayManager.cs
+++ MedicalKanda/Assets/TrayManager.cs
@@ -4,27 +4,54 @@ using UnityEngine;
 
 public class TrayManager : MonoBehaviour
 {
+public GameObject objectToTurnGreen; // Drag the object in the inspector
+public Material greenMaterial; // Drag the green material in the inspector
     private int organsPlacedCorrectly = 0;
+private bool doorOpened = false;
 
-    public void OrganPlacedCorrectly()
+public void OrganPlacedCorrectly()
+{
+    organsPlacedCorrectly++;
+    Debug.Log("Organs placed correctly: " + organsPlacedCorrectly);
+
+    if (organsPlacedCorrectly >= 3)
     {
-        organsPlacedCorrectly++;
-        Debug.Log("Organs placed correctly: " + organsPlacedCorrectly);
+        OpenDoor();
+    }
+}
 
-        if (organsPlacedCorrectly >= 3)
+public void OrganRemoved()
+{
+    organsPlacedCorrectly--;
+    Debug.Log("Organs placed correctly: " + organsPlacedCorrectly);
+}
+
+private void OpenDoor()
+{
+    if (doorOpened)
+    {
+        Debug.Log("Door already opened");
+        return;
+    }
+
+    // Get the door game object
+    GameObject door = GameObject.Find("DoorPivot");
+
+    // Rotate the door by -90 degrees on the x-axis and y-axis
+    door.transform.Rotate(new Vector3(0f, -90f, 0f));
+
+    Debug.Log("Door opened");
+
+    doorOpened = true;
+
+    // Change the material of the specified game object
+    if (objectToTurnGreen != null)
+    {
+        Renderer renderer = objectToTurnGreen.GetComponent<Renderer>();
+        if (renderer != null)
         {
-            OpenDoor();
+            renderer.material = greenMaterial;
         }
     }
-
-    public void OrganRemoved()
-    {
-        organsPlacedCorrectly--;
-        Debug.Log("Organs placed correctly: " + organsPlacedCorrectly);
-    }
-
-    private void OpenDoor()
-    {
-        // Code to open the door goes here
-    }
+}
 }
